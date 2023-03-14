96eb8f0fb377f0b60fbc85696727a3ceab8cf46e
Will Adams <bushblade@gmail.com>
2023-03-14 18:46:06 +0000

    navbar scaffold pages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        fira: ['Fira Mono', ...defaultTheme.fontFamily.mono],
+      },
+    },
   },
-  plugins: [require('daisyui')],
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
   daisyui: {
-    themes: ['forest'],
+    themes: ['night'],
   },
 }
