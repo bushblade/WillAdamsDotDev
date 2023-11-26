98986530487c21fe575e88dee1f1306842e0465d
Will Adams <bushblade@gmail.com>
2023-11-26 16:41:14 +0000

    fade in images from low quality source

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,15 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
+      animation: {
+        'fade-in': 'fadeIn 0.5s ease-out',
+      },
       colors: {
         // DaisyUI removed this colour, so adding it back
         'neutral-focus': '#0d1c17',
